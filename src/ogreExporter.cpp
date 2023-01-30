5ecb15b229b40c1a95ea7ebde110ed9b96dd92b5
Chris Tallman <chris.tallman@surgicalscience.com>
2023-01-30 15:56:26 -0800

    TOOL-83: Bump version number.

--- src/ogreExporter.cpp
+++ src/ogreExporter.cpp
@@ -23,10 +23,10 @@
 - maintained by Bitgate, Inc. for the purpose of keeping Ogre compatible with the latest	-
 - technologies.																				-
 ---------------------------------------------------------------------------------------------
-- Copyright (c) 2011 MFG Baden-Württemberg, Innovation Agency for IT and media.             -
+- Copyright (c) 2011 MFG Baden-Wï¿½rttemberg, Innovation Agency for IT and media.             -
 - Research and Development at the Institute of Animation is a cooperation between           -
-- MFG Baden-Württemberg, Innovation Agency for IT and media and                             -
-- Filmakademie Baden-Württemberg as part of the "MFG Visual Experience Lab".                -
+- MFG Baden-Wï¿½rttemberg, Innovation Agency for IT and media and                             -
+- Filmakademie Baden-Wï¿½rttemberg as part of the "MFG Visual Experience Lab".                -
 ---------------------------------------------------------------------------------------------
 - This program is free software; you can redistribute it and/or modify it under				-
 - the terms of the GNU Lesser General Public License as published by the Free Software		-
@@ -1128,7 +1128,7 @@ namespace OgreMayaExporter
 MStatus initializePlugin( MObject obj )
 {
 	MStatus   status;
-	MFnPlugin plugin( obj, "OgreExporter", "v1.0.0-github", "Any");
+	MFnPlugin plugin( obj, "OgreExporter", "v2023.1", "Any");
 	status = plugin.registerCommand( "ogreExport", OgreMayaExporter::OgreExporter::creator );
 	if (!status) {
 		status.perror("registerCommand");
